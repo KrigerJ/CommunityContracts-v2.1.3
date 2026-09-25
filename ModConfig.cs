017cad051ffd9575c48c55e255e185b23abe4a30
KrigerJ <jjjensen65@gmail.com>
2026-09-24 18:15:27 -0700

    Add files via upload

---
+++ ModConfig.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using static CommunityContracts.Core.NPCServiceMenu;
+using static CommunityContracts.Core.Services.WarpMenu;
+
+public class ModConfig
+{
+    public SButton CheatMenuHotkey { get; set; } = (SButton)86;
+    public string DeliveryChestColor { get; set; } = "White";
+    public string HighlightColor { get; set; } = "Yellow";
+    public string FontColor { get; set; } = "Black";
+
+    public Dictionary<string, int> NPCContractPercents { get; set; } = new()
+    {
+        { "Basic", 80 },
+        { "Custom", 70 },
+        { "Processing", 3 },
+    };
+
+    public Dictionary<string, int> CraftablFee { get; set; } = new()
+    {
+        { "GardenPot", 400 },
+        { "BeeHouse", 2000 },
+        { "Tapper", 900 },
+        { "Bait", 5 },
+    };
+
+    public Dictionary<ServiceId, int> SeviceContractFees { get; set; } = new()
+    {
+        { ServiceId.SetCrabPots, 100 },
+        { ServiceId.BaitCrabPots, 2 },
+        { ServiceId.CrabPots, 3 },
+        { ServiceId.Crops, 4 },
+        { ServiceId.Forageables, 3 },
+        { ServiceId.Hardwood, 5 },
+        { ServiceId.Honey, 5 },
+        { ServiceId.Stone, 4 },
+        { ServiceId.Weeds, 2 },
+        { ServiceId.Wood, 4 },
+        { ServiceId.Tappers, 3 },
+        { ServiceId.Producers, 3 },
+        { ServiceId.Till, 5 },
+        { ServiceId.Water, 1 },
+        { ServiceId.Fertilize, 1 },
+        { ServiceId.Seeds, 3 },
+        { ServiceId.PlaceInvisiblePots, 100 },
+        { ServiceId.PlaceBeeHouse, 100 },
+        { ServiceId.SeedMaker, 10 },
+        { ServiceId.Animals, 5 },
+        { ServiceId.Warp, 5 },
+        { ServiceId.RockCandy, 12 },
+        { ServiceId.Processing, 2 }
+    };
+
+    public Dictionary<string, WarpLocationEntry> WarpLocations { get; set; } = new()
+    {
+        ["Home"] = new WarpLocationEntry { Map = "Farm", X = 64, Y = 15 },
+    };
+
+    public float ConeAngleDegrees { get; set; } = 90f;
+    public int ConeRange { get; set; } = 90;
+    public int RectangleWidth { get; set; } = 7;
+    public int RectangleLength { get; set; } = 5;
+    public bool PlaceNearToFar = false;
+    public int MenuColumns { get; set; } = 5;
+    public int CollectionDelay { get; set; } = 2000;
+    public string DropLocationName { get; set; } = "Farm";
+    public int DropTileX { get; set; } = 68;
+    public int DropTileY { get; set; } = 15;
+    public bool BombActive { get; set; } = false;
+    public bool EnableProcessTimeReduction = false;
+    public bool ReturnDamage { get; set; } = false;
+    public bool FriendShield{ get; set; } = false;
+    public bool NPCOrderDescending { get; set; } = true;
+    public int RetDamgFee { get; set; } = 1;
+
+    public Dictionary<string, Color> ChestColors { get; set; } = new()
+    {
+        { "White", Color.White },
+        { "Sky Blue", Color.LightSkyBlue },
+        { "Gold", Color.Gold },
+        { "Green", Color.LightGreen },
+        { "Purple", Color.MediumPurple },
+        { "Orange", Color.Orange },
+        { "Pink", Color.Pink }
+    };
+    public Dictionary<string, Color> HighlightColors { get; set; } = new()
+    {
+        { "Yellow", Color.Yellow * 0.75f },
+        { "Red", Color.Red * 0.6f },
+        { "Green", Color.LimeGreen * 0.6f },
+        { "Blue", Color.DeepSkyBlue * 0.6f },
+        { "White", Color.White * 0.5f },
+        { "Purple", Color.MediumPurple * 0.6f }
+    };
+    public Dictionary<string, Color> FontColors { get; set; } = new()
+    {
+        { "Black", Color.Black },
+        { "White", Color.White },
+        { "Yellow", Color.Yellow },
+        { "Red", Color.Red },
+        { "Green", Color.LimeGreen },
+        { "Blue", Color.DeepSkyBlue },
+        { "Purple", Color.MediumPurple }
+    };
+
+    public bool FuelLockerAssigned { get; set; } = false;
+    public string FuelLockerLocation { get; set; } = "";
+    public string FuelLockerTile { get; set; } = "";
+    public bool ProcessingLockerAssigned { get; set; } = false;
+    public string ProcessingLockerLocation { get; set; } = "";
+    public string ProcessingLockerTile { get; set; } = "";
+    public bool AllowEmptyTilePotPlacement { get; set; } = false;
+    public List<string> ExcludedItemIds { get; set; } = new()
+    {
+        "(O)92", // Sap by default
+        "(O)388", // Wood by default
+        "(O)390", // Stone by default
+        "(O)0", // Weeds by default
+        "0", // Weeds by default
+    };
+}
---
+++ ModEntry.cs
@@ -0,0 +1,404 @@
+﻿using CommunityContracts.Core;
+using CommunityContracts.Core.PowerUps;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Objects;
+using static CommunityContracts.Core.CollectionHelpers;
+using static CommunityContracts.Core.ContractUtilities;
+using static CommunityContracts.Core.PowerUps.PowerHelpers;
+using SObject = StardewValley.Object;
+
+public class ModEntry : Mod
+{
+    public static ModEntry Instance { get; private set; }
+
+    public static bool IsSelectingTile = false;
+
+    internal static ModConfig Config;
+
+    public static CollectionServiceManager Services;
+
+    public static IMonitor ModMonitor { get; private set; }
+
+    public static Vector2? HighlightedDropTile;
+
+    public static Dictionary<string, int> npcCooldowns = new();
+
+    public static bool ForceShowDropLocationHighlight = false;
+    public static bool ForceShowProcessChestHighlight = false;
+    public static bool ForceShowFuelChestHighlight = false;
+
+    public static IMonitor monitor;
+
+    public static PowerHelpers FriendPower;
+
+    public static int LastHP;
+    public static bool InitializedHP = false;
+
+    public static bool WaitingForFuelLockerSelection = false;
+    public static bool WaitingForProcessingLockerSelection = false;
+
+    public static bool FastFishingActive = false;
+
+    public static string T(string key, object tokens = null)
+    {
+        var translation = Instance.Helper.Translation.Get(key);
+        if (tokens != null)
+            translation = translation.Tokens(tokens);
+        return translation.ToString();
+    }
+
+    public static bool ShowPlacementOverlay = false;
+    public bool SetPlacementMenuOpen = false;
+
+    public class ContractDelivery
+    {
+        public List<Item> Items { get; set; }
+        public string Source { get; set; }
+        public long RecipientID { get; set; }
+    }
+
+    private readonly List<ContractDelivery> ContractsDeliveries = new();
+
+    public void QueueContractsDelivery(ContractDelivery delivery)
+    {
+        if (delivery.Items.Count > 0)
+        {
+            ContractsDeliveries.Add(delivery);
+            //Monitor.Log(T("QueuedDelivery", new { source = delivery.Source }), LogLevel.Info);
+        }
+    }
+
+    private void OnRenderedHud(object sender, RenderedHudEventArgs e)
+    {
+
+    }
+
+    private void OnRenderingHud(object sender, RenderingHudEventArgs e)
+    {
+        if (Config.EnableProcessTimeReduction)
+        {
+            Vector2 cursorTile = Game1.currentCursorTile;
+
+            if (Game1.currentLocation != null &&
+                Game1.currentLocation.Objects.TryGetValue(cursorTile, out SObject obj) &&
+                obj is not null &&
+                obj.bigCraftable.Value &&
+                obj.minutesUntilReady.Value > 0)
+            {
+                int minutesLeft = obj.minutesUntilReady.Value;
+
+                int hours = minutesLeft / 60;
+                int minutes = minutesLeft % 60;
+
+                string tooltip = T("DisplayTime", new { DisName = obj.DisplayName, Hrs = hours, Min = minutes });
+
+                IClickableMenu.drawHoverText(
+                    e.SpriteBatch,
+                    tooltip,
+                    Game1.smallFont
+                );
+            }
+        }
+    }
+
+    public override void Entry(IModHelper helper)
+    {
+        Instance = this;
+
+        ModMonitor = this.Monitor;
+
+        monitor = this.Monitor;
+
+        Config = helper.ReadConfig<ModConfig>();
+
+        Services = new CollectionServiceManager();
+
+        Helper.Events.Display.RenderingHud += OnRenderingHud;
+
+        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+
+        helper.Events.GameLoop.DayStarted += OnDayStarted;
+
+        helper.Events.Input.ButtonPressed += OnButtonPressed;
+
+        helper.Events.Display.MenuChanged += OnMenuChanged;
+
+        helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+
+        helper.Events.Display.RenderedHud += OnRenderedHud;
+
+        helper.Events.GameLoop.TimeChanged += OnTimeChanged;
+
+        Helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+
+        Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+
+        Helper.Events.Display.RenderedWorld += OnRenderedWorld;
+
+        ShockBombs.Helper = helper;
+
+        helper.Events.Input.ButtonPressed += ShockBombs.OnButtonPressed;
+
+        FriendPower = new PowerHelpers(Config);
+
+        helper.Events.GameLoop.SaveLoaded += (_, _) =>
+        {
+            foreach (var location in Game1.locations)
+            {
+                foreach (var pair in location.objects.Pairs)
+                {
+                    if (pair.Value is Chest chest &&
+                        chest.modData.TryGetValue("CommunityContracts/DeliveryColor", out var savedColor) &&
+                        Config.ChestColors.TryGetValue(savedColor, out var tint))
+                    {
+                        chest.playerChoiceColor.Value = tint;
+                    }
+                }
+            }
+        };
+
+        foreach (var m in typeof(Crop).GetMethods())
+        {
+            if (m.Name == "harvest")
+                Monitor.Log($"HARVEST METHOD: {m}", LogLevel.Warn);
+        }
+
+        var QualityHarmony = new Harmony(this.ModManifest.UniqueID);
+
+        Helper.Events.GameLoop.Saving += OnSaving;
+
+        var harmony = new Harmony(this.ModManifest.UniqueID);
+        harmony.PatchAll();
+
+        Helper.Events.GameLoop.DayEnding += (s, e) =>
+        {
+            var convertedDeliveries = ContractsDeliveries
+                .Select(w => new ContractsDelivery
+                {
+                    Items = w.Items,
+                    RecipientID = w.RecipientID
+                })
+                .ToList();
+
+            DeliverContractsItems(convertedDeliveries, Config);
+            ContractsDeliveries.Clear();
+        };
+    }
+
+    private void OnGameLaunched(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnDayStarted(object sender, DayStartedEventArgs e)
+    {
+        FriendPower.Recalculate();
+
+        if (Config.EnableProcessTimeReduction)
+        {
+            ApplyProductionTimeReduction(Config, Monitor);
+            Game1.player.Money -= FriendPower.DailyTimeReductionFee;
+        }
+
+        if (Config.ReturnDamage)
+        {
+            Game1.player.Money -= FriendPower.DailyReturnFee;
+        }
+
+        if (Config.FriendShield)
+        {
+            Game1.player.Money -= FriendPower.DailyShieldFee;
+        }
+
+        foreach (var loc in Game1.locations)
+        {
+            foreach (var obj in loc.objects.Values)
+            {
+                if (obj is Chest chest && chest.modData.ContainsKey("cc.processingChest"))
+                {
+                }
+            }
+        }
+    }
+
+    private void OnSaving(object sender, SavingEventArgs e)
+    {
+
+    }
+
+    private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+    {
+
+    }
+
+    private void OnMenuChanged(object sender, MenuChangedEventArgs e)
+    {
+        if (WaitingForFuelLockerSelection)
+        {
+            if (e.NewMenu is ItemGrabMenu igm && igm.context is Chest chest)
+            {
+                WaitingForFuelLockerSelection = false;
+
+                Config.FuelLockerAssigned = true;
+                Config.FuelLockerLocation = chest.Location.Name;
+                Config.FuelLockerTile = $"{(int)chest.TileLocation.X},{(int)chest.TileLocation.Y}";
+
+                Helper.WriteConfig(Config);
+
+                Game1.addHUDMessage(new HUDMessage(T("FuelLockerAssigned"), HUDMessage.newQuest_type));
+            }
+        }
+
+        else
+
+            if (WaitingForProcessingLockerSelection)
+            {
+                if (e.NewMenu is ItemGrabMenu igmenu && igmenu.context is Chest ProcessingLocker)
+                {
+                    WaitingForProcessingLockerSelection = false;
+
+                    Config.ProcessingLockerAssigned = true;
+                    Config.ProcessingLockerLocation = ProcessingLocker.Location.Name;
+                    Config.ProcessingLockerTile = $"{(int)ProcessingLocker.TileLocation.X},{(int)ProcessingLocker.TileLocation.Y}";
+
+                    ProcessingLocker.modData["cc.processingChest"] = "true";
+
+                    Helper.WriteConfig(Config);
+
+                    Game1.addHUDMessage(new HUDMessage(T("ProcessLockerAssigned"), HUDMessage.newQuest_type));
+                }
+            }
+
+            else
+                return;
+    }
+
+    private void OnTimeChanged(object sender, TimeChangedEventArgs e)
+    {
+        foreach (var key in npcCooldowns.Keys.ToList())
+        {
+            if (npcCooldowns[key] > 0)
+                npcCooldowns[key] -= 10;
+
+            if (npcCooldowns[key] <= 0)
+                npcCooldowns.Remove(key);
+        }
+    }
+
+    private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+    {
+        if (!Context.IsWorldReady)
+            return;
+
+        Farmer farmer = Game1.player;
+
+        if (Instance.SetPlacementMenuOpen)
+        {
+            if (Game1.activeClickableMenu is not SetPlacement)
+            {
+                ShowPlacementOverlay = false;
+                Instance.SetPlacementMenuOpen = false;
+            }
+        }
+
+        if (!InitializedHP)
+        {
+            LastHP = farmer.health;
+            InitializedHP = true;
+            return;
+        }
+
+        if (farmer.health < LastHP)
+        {
+            int rawDamage = LastHP - farmer.health;
+            int finalDamage = rawDamage;
+
+            if (Config.FriendShield)
+            {
+                int reduced = (int)(rawDamage * (1f - FriendPower.ShieldPercent));
+                finalDamage = Math.Max(1, reduced);
+
+                int healBack = rawDamage - finalDamage;
+                if (healBack > 0)
+                    farmer.health += healBack;
+            }
+
+            if (Config.ReturnDamage && FriendPower.ReturnPercent > 0f)
+            {
+                TriggerReturnDamageExplosions(finalDamage);
+            }
+        }
+
+        LastHP = farmer.health;
+    }
+
+    private void OnRenderedWorld(object sender, RenderedWorldEventArgs e)
+    {
+        if (ForceShowDropLocationHighlight || ForceShowProcessChestHighlight || ForceShowFuelChestHighlight)
+        {
+            DrawDeliveryLocationHighlight(
+                e.SpriteBatch,
+                Config.DropLocationName,
+                Config,
+                s => T(s)
+            );
+        }
+
+        if (ShowPlacementOverlay)
+                DrawSquarePlacementOverlay(e.SpriteBatch);
+    }
+
+    public static void AssignFuelLocker()
+    {
+        WaitingForFuelLockerSelection = true;
+        Game1.addHUDMessage(new HUDMessage(T("AssignFuelLocker"), HUDMessage.newQuest_type));
+    }
+
+    public static void AssignProcessLocker()
+    {
+        WaitingForProcessingLockerSelection = true;
+        Game1.addHUDMessage(new HUDMessage(T("AssignProcessLocker"), HUDMessage.newQuest_type));
+    }
+
+    private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+    {
+        if (e.Button == SButton.N)
+        {
+        }
+
+        if (!Context.IsWorldReady)
+            return;
+
+        if (!Context.IsPlayerFree)
+            return;
+
+        if (Config.CheatMenuHotkey != SButton.None && e.Button == Config.CheatMenuHotkey)
+        {
+            Game1.activeClickableMenu = new CCToolbar(Helper, Monitor, Config);
+        }
+
+        if (e.Button == SButton.MouseLeft)
+        { 
+            Vector2 cursorPos = new Vector2(Game1.getMouseX(), Game1.getMouseY());
+        } 
+
+        if (!Context.IsWorldReady || !IsSelectingTile || e.Button != SButton.MouseLeft)
+            return;
+
+        var cursorTile = e.Cursor.Tile;
+        var locationName = Game1.currentLocation.Name;
+
+        Config.DropLocationName = locationName;
+        Config.DropTileX = (int)cursorTile.X;
+        Config.DropTileY = (int)cursorTile.Y;
+        Helper.WriteConfig(Config);
+
+        Game1.addHUDMessage(new HUDMessage(T("DeliveryLocationSet", new { location = locationName, x = cursorTile.X, y = cursorTile.Y }), HUDMessage.newQuest_type));
+        IsSelectingTile = false;
+    }
+}
